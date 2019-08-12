37881222072a1af9f3e67a22d0c55352a6d81d8e
Victor <victorivanov.spb@gmail.com>
2019-08-12 18:10:08 +0300

    Add clean-webpack-plugin
    Плагином выполняется автоматическая чистка файлов в директории ./dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');  // https://github.com/webpack-contrib/mini-css-extract-plugin
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // https://github.com/johnagan/clean-webpack-plugin
 
 module.exports = {
     mode: 'development',
@@ -17,6 +18,9 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
+        }),
+        new CleanWebpackPlugin({
+            path: './dist/*.*'
         })
     ],
     module: {

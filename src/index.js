a1c2550d991b71686348cebd438a7bc5de17c7d8
Victor <victorivanov.spb@gmail.com>
2019-08-12 18:33:05 +0300

    Add plugins: jquery, popper.js. Use webpack.ProvidePlugin.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
+// import 'jquery';
+// import 'popper.js';
 import './css/styles.css';
 
 console.log('Hello, world!');
 console.log('Hello, world! Again.');
+
+jQuery(document)
+    .ready(function($) {
+        console.log('jquery');
+
+        ((selector) => {
+            const color = (Math.random() > 0.5) ? 'light-gray' : 'white';
+            selector.css('background-color', color);
+        })( $('body') );
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');  // https://github.com/webpack-contrib/mini-css-extract-plugin
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // https://github.com/johnagan/clean-webpack-plugin
@@ -21,6 +22,11 @@ module.exports = {
         }),
         new CleanWebpackPlugin({
             path: './dist/*.*'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jquery': 'jquery'
         })
     ],
     module: {
